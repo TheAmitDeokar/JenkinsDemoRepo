df666e55c154e073e0948580f83346690095105a
Amit Deokar <amitdeokar20@gmail.com>
2021-05-19 09:31:22 +0530

    first commit

---
+++ src/main/java/QualitykioskTraining/JenkinsDemo/CalculatorAPI.java
@@ -0,0 +1,12 @@
+package QualitykioskTraining.JenkinsDemo;
+
+public class CalculatorAPI 
+{
+	
+	public int Addition(int num1,int num2)
+	{
+		int Res=num1+num2;
+		return Res;
+	}
+
+}
---
+++ src/test/java/QualitykioskTraining/JenkinsDemo/TestAddition.java
@@ -0,0 +1,42 @@
+package QualitykioskTraining.JenkinsDemo;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class TestAddition 
+{
+
+	CalculatorAPI Obj;
+	int R;
+	
+	
+	//@Before("RegressionTest")
+	public void InitGroup()
+	{
+		System.out.println("Im in Before class");
+		Obj=new CalculatorAPI();	
+	}
+	
+	@BeforeClass
+	public void Init()
+	{
+		System.out.println("Im in Before class");
+		Obj=new CalculatorAPI();
+	}
+	@BeforeMethod
+	public void ReinitialiseResultVar()
+	{
+		System.out.println("I am in Before Method");
+		R=0;
+	}
+	@Test(priority=1)
+	public void TestAdditionWithPositiveNumbers()
+	{
+		System.out.println("I am in 1 st TestCase");
+		R=Obj.Addition(10,20);
+		Assert.assertEquals(R, 30,"Addition does not work with positive numbers");
+	}
+}
